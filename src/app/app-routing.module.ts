72af42bbd6893d0d201a204affe71186afedb80b
Sruthi57 <53509871+Sruthi57@users.noreply.github.com>
2020-10-04 12:11:14 +0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AdminMainComponent } from './Main/admin-main/admin-main.component';
+import { ManagerMainComponent } from './Main/manager-main/manager-main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'admin', component: AdminMainComponent },
+  { path: 'manager', component: ManagerMainComponent },
+  { path: 'user', component: AdminMainComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
